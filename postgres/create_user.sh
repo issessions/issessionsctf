69e484cc596915e1c3231152a2b684fd2b247697
SpencerLee <spacecar@spencer.fedora>
2020-02-28 12:13:31 -0500

    removed unessisary vars

--- postgres/create_user.sh
+++ postgres/create_user.sh
@@ -1,1 +1,1 @@
-psql -U postgres -v DB_NAME=$DB_NAME -v DB_USER=$DB_USER -v DB_PASS=$DB_PASS -v DB_ADDRES=$DB_ADDRES -v DB_PORT=$DB_PORT -f /home/create_user.sql
+psql -U postgres -v DB_NAME=$DB_NAME -v DB_USER=$DB_USER -v DB_PASS=$DB_PASS -f /home/create_user.sql
