604d2c82041fe7fefc9eb3cb2056cf4f780a4158
SpencerLee <spacecar@localhost.localdomain>
2020-03-05 13:16:47 -0500

    fixed import script

--- django/challenge_import.sh
+++ django/challenge_import.sh
@@ -6,19 +6,32 @@
 
 #Removes first line of CSV file, and starts the import statments @ line 2
 function import_csv {
-	sed 1d $1 | while IFS=, read -r challenge_name developer_name description_file_name points link participant_zip_name deployement_zip_name hint hint_penalty flag category solution_file_name web_project docker
+	sed 1d $1 | while IFS=, read -r challengeid challenge_name developer_name description_file_name points link participant_zip_name deployement_zip_name hint hint_penalty flag category solution_file_name web_project docker
 	do
 		category=$(echo $category | awk '{ print toupper($0) }')
-		psql -U postgres -h postgresql -d iss -c "INSERT INTO ctf_challenge (contest_id, name, description, category, link, file, active, dynamic_link, sponsored) values ('1', '$challenge_name', '$description_file_name', '$category', '$link', '$participant_zip_name', true, false, false) RETURNING ctf_challenge.id; INSERT INTO ctf_flag (name, flag, points, hint, penalty, solved, last_solved, challenge_id) VALUES ('$challenge_name', '$flag', '$points', '$hint',  '$hint_penalty', 0, NULL, currval('ctf_challenge_id_seq'))"
+		if echo $link | grep -i "n/a"; then
+			link=""
+		fi
+		description_file_content=$(cat $2/$challengeid/$description_file_name)
+		hint_content=$(cat $2/$challengeid/$hint)
+		solution_file_name_content=$(cat $2/$challengeid/$solution_file_name)
+
+		psql -U postgres -h postgresql -d iss -c "INSERT INTO ctf_challenge (contest_id, name, description, category, link, file, active, dynamic_link, sponsored) values ('1', '$challenge_name', '$description_file_content', '$category', '$link', '$participant_zip_name', true, false, false) RETURNING ctf_challenge.id; INSERT INTO ctf_flag (name, flag, points, hint, penalty, solved, last_solved, challenge_id) VALUES ('$challenge_name', '$flag', '$points', '$hint_content',  '$hint_penalty', 0, NULL, currval('ctf_challenge_id_seq'))"
 	done
 }
 
 contest_created=0
-for i in $(find /usr/src/app/ -name "*.csv"); do
-	if [ $contest_created == 0 ]; then
+if [ $(find /usr/src/app/ -name "*.zip") ]; then
+#for i in $(find /usr/src/app/ -name "*.csv"); do
+	if [[ $CREATE_CONTEST == 1 && $contest_created == 0 ]]; then
+		echo "Creating Contest Name: $CONTEST_NAME"
 		#Creates contest, in order for challenge imports to work there needed to be a contest with the ID of 1
 		psql -U postgres -h postgresql -d iss -c "INSERT INTO ctf_contest values(1, '$CONTEST_NAME', '$CONTEST_ACTIVE', $CONTEST_START::TIMESTAMPTZ, $CONTEST_END::TIMESTAMPTZ)"
 		contest_created=1
 	fi
-        import_csv $i;
-done
+	challenge_folder=$(find /usr/src/app/ -name "*.zip" | sed 's/.zip//g')
+	unzip $(find /usr/src/app/ -name "*.zip")
+	for i in $(find $challenge_folder -name "*.csv"); do
+        	import_csv $i $challenge_folder;
+	done
+fi
