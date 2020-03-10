72b35c2caed8af57c9791e2e0e2797779307defb
Louai Abboud <abboudl@sheridancollege.ca>
2020-03-10 11:51:05 -0400

    Initial Commit

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker-compose up -d --build --force-recreate
-#python3 -m venv ./django/venv && source ./django/venv/bin/activate && pip install -r ./django/requirements.txt
+docker-compose up -d
 chown -R $SUDO_USER:$SUDO_USER ./django/
 
