3e967f3e15d8496aeeda2e5f6e7be3371bbb2970
Louai Abboud <abboudl@sheridancollege.ca>
2020-03-12 02:10:40 -0400

    fixed a text rendering issue with django templates

--- django/django.sh
+++ django/django.sh
@@ -6,18 +6,4 @@ until pg_isready -h $DB_ADDRESS ; do
   sleep 3
 done
 
->&2 echo "Postgres is up - executing commands"
-
->&2 echo -e "Checking if the postgresql database is already setup\n"
-psql -h $DB_ADDRESS -U $DB_USER -d $DB_NAME -c "select count(*) from ctf_challenge" > /dev/null 2>&1
-if [ "$?" == "1" ]; then
-  >&2 echo -e "Setting up postgresql database\n"
-  python3 manage.py makemigrations ctf
-  python3 manage.py migrate
-  echo "from django.contrib.auth.models import User; User.objects.create_superuser('$ADMIN_USER', '$ADMIN_USER@localhost', '$ADMIN_PASSWORD')" | python3 manage.py shell
-  #bash challenge_import.sh
-else
-  >&2 echo "Postgresql database already setup"
-fi
-
 gunicorn issessionsctf.wsgi:application --workers=5 -b 0.0.0.0:80
